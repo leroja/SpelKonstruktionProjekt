d6015c4323abf20a468dac690013df658ba3dd43
Lennart Jakobsson <len.nart@hotmail.com>
2016-04-19 14:27:45 +0200

    renamed some stuff

--- Spel/Spel/Program.cs
+++ Spel/Spel/Program.cs
@@ -14,7 +14,7 @@ namespace Spel
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Game())
                 game.Run();
         }
     }
