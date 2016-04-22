cbcc6d69ef1777598eebd3a87b2e71451bd855d3
AllePihl <alle.pihl@gmail.com>
2016-04-22 13:38:21 +0200

    Added GravitationComponent

---
+++ Spel/GameEngine/Source/Components/GravitationComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Components
+{
+    // GravitationComponent for Gravity
+    class GravitationComponent
+    {
+        // Flot representing Gravity
+        float gravity { get; set; }
+         
+        // Constructor form gravitationComponent
+        public GravitationComponent(float gravity)
+        {
+            this.gravity = gravity;
+        }
+    }
+}
