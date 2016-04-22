6cba28f4561249b10b6058bb31a636d7f37b72d6
AllePihl <alle.pihl@gmail.com>
2016-04-22 11:04:22 +0200

    Component & Health Component

---
+++ Spel/GameEngine/Source/Components/HealthComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Components
+{
+    // Health Component
+    public class HealthComponent : Component
+    {
+        // Max health
+        int Health;
+        // Damage taken
+        int Damage;
+    }
+}
