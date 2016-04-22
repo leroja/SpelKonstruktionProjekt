489846a51893edc7736d053d05237d167d2380f1
Thires Nilsson <zzoooki@gmail.com>
2016-04-22 13:37:39 +0200

    hej

--- Spel/GameEngine/Source/Entity/Entity.cs
+++ Spel/GameEngine/Source/Entity/Entity.cs
@@ -8,19 +8,17 @@ namespace GameEngine.Source.Entity
 {
     /// <summary>
     /// The Entity class is the objects which the components will be applied to. 
-    /// The entities contains a ID which is uniqe for each entity in order to find 
-    /// specific enteties. Therefore the id when constructing new entities should be uniqe.
     /// </summary>
     class Entity
     {
-        int entityID { get; set; }
+        public bool visable { get; set; }
+        public bool updateable { get; set; }
         /// <summary>
         /// Default constructor for the Entity class.
         /// </summary>
-        /// <param name="id">Takes a ID which is uniqe for each entity in order to find 
-        /// specific enteties. Therefore the id when constructing new entities should be uniqe.</param>
-        Entity(int id) {
-            entityID = id;
+        Entity() {
+            visable = true;
+            updateable = true;
         }
     }
 }
