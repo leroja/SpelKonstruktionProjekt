d3d448e4c01ab2d22d6bd7fdd3e86293ac5a6d3e
RasmusLundquist <Rasmus.lundquist@hotmail.com>
2016-04-22 11:14:29 +0200

    VelocityComponent added

---
+++ Spel/GameEngine/Source/Components/VelocityComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace GameEngine.Source.Components
+{
+    class VelocityComponent
+    {
+        public Vector2 velocity { get; set; }
+        public float speed { get; set; }
+        public int direction { get; set; }
+
+        public VelocityComponent(Vector2 velocity, float speed)
+        {
+            this.velocity = velocity;
+            this.speed = speed;
+        }
+    }
+}
