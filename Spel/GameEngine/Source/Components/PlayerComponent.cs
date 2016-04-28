d6cdef9312f9c65a6dca995db638285754954f33
Thires Nilsson <zzoooki@gmail.com>
2016-04-28 11:19:30 +0200

    Created player component

---
+++ Spel/GameEngine/Source/Components/PlayerComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Components
+{
+
+    class PlayerComponent
+    {
+        private string playerName;
+        private int playerNumber;
+
+        public PlayerComponent(string playerName)
+        {
+            this.playerName = playerName;
+        }
+        public PlayerComponent(int playerNumber)
+        {
+            playerName = "Player" + playerNumber;
+        }
+        public PlayerComponent()
+        {
+            playerName = "Player" + new Random().Next(1, 999999999);
+        }
+    }
+}
