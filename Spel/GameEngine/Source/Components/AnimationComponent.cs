6c97d933c83bf1378832c858d3c739330047b2f1
Thires Nilsson <zzoooki@gmail.com>
2016-05-03 11:03:21 +0200

    Added comments to the animation component functions

--- Spel/GameEngine/Source/Components/AnimationComponent.cs
+++ Spel/GameEngine/Source/Components/AnimationComponent.cs
@@ -17,7 +17,7 @@ namespace GameEngine.Source.Components
 
         /// the number of frames per second to be drawn
         /// we need to tell our spritesheet how many frames to wait before transitioning.
-        public double framesPerSecond { get; set; }
+        public double timePerFrame { get; set; }
         public int currentFrame { get; set; }
 
         /// the elapsed time since the last frame increment, and other options.
@@ -31,9 +31,17 @@ namespace GameEngine.Source.Components
 
         public Rectangle sourceRectangle = new Rectangle();
 
-        public AnimationComponent(int animationSizeWidth, int animationSizeHeight, int textureWidth, int textureHeight, double framesPerSecond)
+        /// <summary>
+        /// AnimationComponent constructor
+        /// </summary>
+        /// <param name="animationSizeWidth">is an int which is the desired width of one frame in the animation</param>
+        /// <param name="animationSizeHeight">is an int which is the desired height of one frame in the animation</param>
+        /// <param name="textureWidth">is an int which is the total with of the 2D texture</param>
+        /// <param name="textureHeight">is an int which is the total height of the 2D texture</param>
+        /// <param name="timePerFrame">is a double which is the time that should be passed before changing the frame.</param>
+        public AnimationComponent(int animationSizeWidth, int animationSizeHeight, int textureWidth, int textureHeight, double timePerFrame)
         {
-            this.framesPerSecond = framesPerSecond;
+            this.timePerFrame = timePerFrame;
             numFramesInColumn = textureWidth / animationSizeWidth;
             numFramesInRow = textureHeight / animationSizeHeight;
             sourceRectangle.Width = animationSizeWidth;
@@ -41,7 +49,11 @@ namespace GameEngine.Source.Components
             this.currentFrame = 0;
         }
 
-        public void setNewPosRectangele(int frame)
+        /// <summary>
+        /// setNewPosRectangle function is used to set a new position for the animation rectangle in order to change frame.
+        /// </summary>
+        /// <param name="frame">is a int variable which sould be representing the current frame that we're at.</param>
+        public void setNewPosRectangle(int frame)
         {
             int y = frame / numFramesInColumn;
             int x = frame - (y * numFramesInColumn);
@@ -49,6 +61,10 @@ namespace GameEngine.Source.Components
             sourceRectangle.Y = y * sourceRectangle.Height;
         }
 
+        /// <summary>
+        /// getAnimationLength method returns the length of the animation, this is calculated by the animationFramesInRow * animationFramesInColumn.
+        /// </summary>
+        /// <returns>returns an int which is the number of frames in the animation.</returns>
         public int getAnimationLength()
         {
             return numFramesInColumn * numFramesInRow;
