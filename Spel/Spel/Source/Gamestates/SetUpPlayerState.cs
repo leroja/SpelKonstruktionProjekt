04397c5542dacf5e5d78a40e6c9ecd269717a5ed
Thires Nilsson <zzoooki@gmail.com>
2016-05-04 09:51:45 +0200

    Started building our gamestates and an interface for these. Also added a new constructor for the animation component

---
+++ Spel/Spel/Source/Gamestates/SetUpPlayerState.cs
@@ -0,0 +1,24 @@
+﻿using GameEngine.Source.Systems.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel.Source.Gamestates
+{
+    class SetUpPlayerState : IGamestate
+    {
+        public List<int> entetiesInState { get; set; }
+
+        /// <summary>
+        /// SetUpPlayerState constructor, which is responsible for adding enteties to the scene in the gameplay where the players choose their caracters and
+        /// controlls etc.
+        /// </summary>
+        public SetUpPlayerState()
+        {
+            //add the enteties which should be displayed on the screen when the players choose their caracters. Player enteties is to be created after leaving this state therefore the 
+            //add entetiestolist-function needs to be called before entering the playing-state
+        }
+    }
+}
