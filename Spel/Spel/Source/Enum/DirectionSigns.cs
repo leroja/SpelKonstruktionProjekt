9b2eeb6828685d72b59699ee6a8b4a944e3e884f
AllePihl <alle.pihl@gmail.com>
2016-05-24 15:36:55 +0200

    Välj Spelare

---
+++ Spel/Spel/Source/Enum/DirectionSigns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel.Source.Enum
+{
+    public enum DirectionSigns
+    {
+        One , Two , Four , Six , Eight , Ten , Twelve 
+    }
+}
--- Spel/Spel/Source/Gamesceenes/SetUpPlayerScene.cs
+++ Spel/Spel/Source/Gamesceenes/SetUpPlayerScene.cs
@@ -14,6 +14,7 @@ using GameEngine.Source.Components;
 using GameEngine.Source.Managers;
 using Spel.Source.Gamestates;
 using Spel.Source.Gamesceenes;
+using Spel.Source.Enum;
 namespace Spel.Source.Gamestates
 {
     /// <summary>
@@ -22,7 +23,15 @@ namespace Spel.Source.Gamestates
     class SetUpPlayerScene : IGamescene
     {
         public List<int> entitiesInState { get; set; }
+        private List<int> Players;
+        private List<Keys> UnAvailableKeys;
         private int textId;
+        private int Index;
+        private int newId;
+        private Map map;
+        private KeyBoardComponent kbcArrow;
+        private KeyBoardComponent kbc1;
+        private DrawableTextComponent draw;
 
 
         /// <summary>
@@ -32,7 +41,10 @@ namespace Spel.Source.Gamestates
         public SetUpPlayerScene()
         {
             entitiesInState = new List<int>();
-            
+            Players = new List<int>();
+            UnAvailableKeys = new List<Keys>();
+            UnAvailableKeys.Add(Keys.Enter);
+            UnAvailableKeys.Add(Keys.Right);
         }
 
         /// <summary>
@@ -41,6 +53,25 @@ namespace Spel.Source.Gamestates
         /// </summary>
         public void onSceneCreated()
         {
+
+            DrawableTextComponent text1 = new DrawableTextComponent("Map:", Color.Black, Game.Instance.GetContent<SpriteFont>("Fonts/Menufont"));
+            PositionComponent pos1 = new PositionComponent(new Vector2(0, Game.Instance.GraphicsDevice.Viewport.Height - 80));
+            int id1 = ComponentManager.Instance.CreateID();
+            ComponentManager.Instance.AddComponentToEntity(id1, text1);
+            ComponentManager.Instance.AddComponentToEntity(id1, pos1);
+            entitiesInState.Add(id1);
+
+            draw = new DrawableTextComponent("Whiteboard", Color.Black, Game.Instance.GetContent<SpriteFont>("Fonts/Menufont"));
+            PositionComponent pos2 = new PositionComponent(new Vector2(105, Game.Instance.GraphicsDevice.Viewport.Height - 80));
+            kbc1 = new KeyBoardComponent();
+            kbc1.keyBoardActions.Add(ActionsEnum.Left, Keys.Right);
+            newId = ComponentManager.Instance.CreateID();
+            ComponentManager.Instance.AddComponentToEntity(newId, draw);
+            ComponentManager.Instance.AddComponentToEntity(newId, pos2);
+            ComponentManager.Instance.AddComponentToEntity(newId, kbc1);
+            entitiesInState.Add(newId);
+
+
             DrawableTextComponent text = new DrawableTextComponent("Press Enter To Start", Color.Black, Game.Instance.GetContent<SpriteFont>("Fonts/Menufont"));
             PositionComponent pos = new PositionComponent(new Vector2(300, 0));
             KeyBoardComponent kbc = new KeyBoardComponent();
@@ -60,11 +91,90 @@ namespace Spel.Source.Gamestates
         {
             Game game = Game.Instance;
             KeyBoardComponent temp = ComponentManager.Instance.GetEntityComponent<KeyBoardComponent>(textId);
+
+            if (kbc1.state[ActionsEnum.Left] == ButtonStates.Pressed)
+            {
+                map = map + 1;
+                if (map > Map.Random)
+                    map = Map.Whiteboard;
+
+                switch (map)
+                {
+                    case Map.Whiteboard:
+
+                        draw.text = "Whiteboard";
+                        break;
+                    case Map.Temp:
+
+                        draw.text = "Temp";
+                        break;
+                    case Map.Random:
+
+                        draw.text = "Random";
+                        break;
+                }
+            }
             if (temp.state[ActionsEnum.Enter] == ButtonStates.Pressed)
             {
                 SceneSystem.Instance.clearScene(entitiesInState);
                 SceneSystem.Instance.setCurrentScene(new PlayingScene());
             }
+            
+
+            KeyboardState tetet = Keyboard.GetState();
+            List<Keys> key = tetet.GetPressedKeys().ToList<Keys>();
+
+            if (key.Count != 0)
+            {
+
+
+                if (UnAvailableKeys != null && !UnAvailableKeys.Contains(key[0]))
+                {
+                    Keys temo = key[0];
+                    key.Clear();
+                    Dictionary<int, Keys> Dir = new Dictionary<int, Keys>();
+                    int tempId = ComponentManager.Instance.CreateID();
+                    int count = Players.Count;
+                    Players.Add(tempId);
+                    DrawableComponent tempDraw = new DrawableComponent(Game.Instance.GetContent<Texture2D>("Pic/kanin1"), SpriteEffects.None);
+                    PositionComponent temppos = new PositionComponent(new Vector2((Game.Instance.GraphicsDevice.Viewport.Width * 0.25f+ 20f) * count, Game.Instance.GraphicsDevice.Viewport.Height * 0.5f));
+                    KeyBoardComponent tempkey = new KeyBoardComponent();
+                    ComponentManager.Instance.AddComponentToEntity(tempId, tempDraw);
+                    ComponentManager.Instance.AddComponentToEntity(tempId, temppos);
+                    ComponentManager.Instance.AddComponentToEntity(tempId, tempkey);
+                    tempkey.keyBoardActions.Add(ActionsEnum.Up, temo);
+                    
+
+                    int textId = ComponentManager.Instance.CreateID();
+                    DrawableTextComponent tempDrawtext = new DrawableTextComponent(temo.ToString(), Color.Black, Game.Instance.GetContent<SpriteFont>("Fonts/MenuFont"));
+                    PositionComponent temptextPos = new PositionComponent(new Vector2((Game.Instance.GraphicsDevice.Viewport.Width * 0.25f+20f) * count, Game.Instance.GraphicsDevice.Viewport.Height * 0.5f + 80f));
+                    ComponentManager.Instance.AddComponentToEntity(textId, tempDrawtext);
+                    ComponentManager.Instance.AddComponentToEntity(textId, temptextPos);
+                    entitiesInState.Add(textId);
+                    UnAvailableKeys.Add(temo);
+                }
+                else
+                {
+                    foreach (var a in Players)
+                    {
+                        KeyBoardComponent tempa = ComponentManager.Instance.GetEntityComponent<KeyBoardComponent>(a);
+                        if (tempa.state[ActionsEnum.Up] == ButtonStates.Pressed)
+                        {
+                            DrawableComponent tempura = ComponentManager.Instance.GetEntityComponent<DrawableComponent>(a);
+                            Random rand = new Random();
+                            tempura.colour = Color.BlueViolet;
+
+                        }
+                    }
+                }
+
+            }
+
+            //rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
+
+
+
+
         }
     }
 }
